kosmosbrewing/biz.shakilabs

fix(seo): use canonical internal home links

client/src/components/layout/AppHeader.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 import { Moon, Sun } from "lucide-vue-next";
-import { RouterLink } from "vue-router";
 import { ShButton } from "@shakilabs/ui";
 import TickerBar from "@/components/common/TickerBar.vue";
 import { tickerMessages } from "@/data/tickerMessages";
@@ -34,8 +33,8 @@ onMounted(() => {
       <div class="overflow-hidden">
         <div class="retro-titlebar h-11 border-b-0 bg-transparent px-1.5 sm:px-2">
           <div class="flex h-full w-full items-center gap-2.5 sm:gap-4">
-            <RouterLink
-              to="/"
+            <a
+              href="/biz"
               aria-label="ShakiLabs 홈"
               class="inline-flex h-8 w-8 shrink-0 items-center justify-center gap-1 px-0.5 text-muted-foreground transition-colors hover:text-foreground sm:w-auto sm:justify-start sm:gap-1.5"
             >
@@ -48,7 +47,7 @@ onMounted(() => {
               <span class="hidden sm:inline font-brand text-tiny font-semibold tracking-wide text-foreground/90">
                 ShakiLabs
               </span>
-            </RouterLink>
+            </a>
             <div class="flex h-full flex-1 items-center justify-center overflow-hidden text-center font-brand text-caption sm:text-body">
               <TickerBar :messages="tickerMessages" />
             </div>

client/src/views/AboutView.vue
@@ -2,7 +2,6 @@
 import SEOHead from "@/components/common/SEOHead.vue";
 import SeoRichGuide from "@/components/common/SeoRichGuide.vue";
 import { BIZ_ABOUT_GUIDE, BIZ_HOME_GUIDE } from "@/data/seoGuides";
-import { RouterLink } from "vue-router";
 import { buttonVariants } from "@/components/ui/button";
 import { useConstantsStore } from "@/stores/constants";
 
@@ -57,9 +56,9 @@ const constantsStore = useConstantsStore();
     </div>
 
     <div class="text-center">
-      <RouterLink :class="buttonVariants({ variant: 'default' })" to="/">
+      <a :class="buttonVariants({ variant: 'default' })" href="/biz">
         홈으로 돌아가기
-      </RouterLink>
+      </a>
     </div>
 
     <SeoRichGuide

client/src/views/NotFoundView.vue
@@ -16,9 +16,9 @@ import { buttonVariants } from "@/components/ui/button";
     <p class="text-heading font-bold text-foreground">페이지를 찾을 수 없습니다</p>
     <p class="text-body text-muted-foreground">입력하신 주소가 잘못되었거나 페이지가 이동되었습니다.</p>
     <div class="flex flex-wrap justify-center gap-2">
-      <RouterLink :class="buttonVariants({ variant: 'default' })" to="/">
+      <a :class="buttonVariants({ variant: 'default' })" href="/biz">
         홈으로 돌아가기
-      </RouterLink>
+      </a>
       <RouterLink :class="buttonVariants({ variant: 'outline' })" to="/individual-vs-corp">
         개인사업자 vs 법인 비교
       </RouterLink>

kosmosbrewing/house.shakilabs

client/src/components/layout/AppHeader.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 import { Moon, Sun } from "lucide-vue-next";
-import { RouterLink } from "vue-router";
 import { ShButton } from "@shakilabs/ui";
 import TickerBar from "@/components/common/TickerBar.vue";
 import { tickerMessages } from "@/data/tickerMessages";
@@ -34,8 +33,8 @@ onMounted(() => {
       <div class="overflow-hidden">
         <div class="retro-titlebar h-[44px] border-b-0 bg-transparent px-1.5 sm:px-2">
           <div class="flex h-full w-full items-center gap-2.5 sm:gap-4">
-            <RouterLink
-              to="/"
+            <a
+              href="/house"
               aria-label="ShakiLabs 홈"
               class="inline-flex h-[44px] w-[44px] shrink-0 items-center justify-center gap-1 px-0.5 text-muted-foreground transition-colors hover:text-foreground sm:h-8 sm:w-auto sm:justify-start sm:gap-1.5"
             >
@@ -48,7 +47,7 @@ onMounted(() => {
               <span class="hidden sm:inline font-brand text-tiny font-semibold tracking-wide text-foreground/90">
                 ShakiLabs
               </span>
-            </RouterLink>
+            </a>
             <div class="flex h-full flex-1 items-center justify-center overflow-hidden text-center font-brand text-caption sm:text-body">
               <TickerBar :messages="tickerMessages" />
             </div>

client/src/views/AboutView.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import SEOHead from "@/components/common/SEOHead.vue";
-import { RouterLink } from "vue-router";
 import { buttonVariants } from "@/components/ui/button";
 import { useConstantsStore } from "@/stores/constants";
 
@@ -55,9 +54,9 @@ const constantsStore = useConstantsStore();
     </div>
 
     <div class="text-center">
-      <RouterLink :class="buttonVariants({ variant: 'default' })" to="/">
+      <a :class="buttonVariants({ variant: 'default' })" href="/house">
         홈으로 돌아가기
-      </RouterLink>
+      </a>
     </div>
   </div>
 </template>

client/src/views/NotFoundView.vue
@@ -16,9 +16,9 @@ import { buttonVariants } from "@/components/ui/button";
     <p class="text-heading font-bold text-foreground">페이지를 찾을 수 없습니다</p>
     <p class="text-body text-muted-foreground">입력하신 주소가 잘못되었거나 페이지가 이동되었습니다.</p>
     <div class="flex flex-wrap justify-center gap-2">
-      <RouterLink :class="buttonVariants({ variant: 'default' })" to="/">
+      <a :class="buttonVariants({ variant: 'default' })" href="/house">
         홈으로 돌아가기
-      </RouterLink>
+      </a>
       <RouterLink :class="buttonVariants({ variant: 'outline' })" to="/delay-interest">
         지연이자 계산
       </RouterLink>